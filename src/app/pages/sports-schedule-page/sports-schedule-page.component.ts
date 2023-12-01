394e390437657290d1c9d94f1083e0a5114179bf
Serhii Levshnia <sergejlevsna@gmail.com>
2023-12-02 00:22:49 +0200

    feature: sport-schedule page styled

--- src/app/pages/sports-schedule-page/sports-schedule-page.component.ts
+++ src/app/pages/sports-schedule-page/sports-schedule-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SortTypes, SportScheduleInfo, WeekDay } from 'src/app/interfaces/data';
 import { DataService } from 'src/app/services/data/data.service';
 
 @Component({
@@ -9,12 +10,55 @@ import { DataService } from 'src/app/services/data/data.service';
 export class SportsSchedulePageComponent {
   constructor(private dataService: DataService) {}
 
-  data: string[] = [];
-  data1 = {};
+  sports: string[] = [];
+  data: SportScheduleInfo[] | [] = [];
   email: string | null = sessionStorage.getItem('email');
+  selectedMenu: string = '';
+  weekDays: WeekDay[] = [
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday',
+    'sunday',
+  ];
+
+  async getSportSchedule(e: any) {
+    const selectedValue = e.target.textContent.trim();
+    await this.getSportScheduleByName(selectedValue);
+    this.selectedMenu = selectedValue;
+  }
 
   getSportsList() {
     this.dataService.getSportsLIst().subscribe(
+      (res) => {
+        this.sports = res;
+      },
+      (err) => {
+        console.error(err, 'Request Error');
+      }
+    );
+  }
+
+  getFilteredDataByDay(day: WeekDay): SportScheduleInfo[] {
+    if (this.data.length > 0) {
+      return this.data.filter((item) => item.day === day);
+    }
+    return [];
+  }
+
+  getSortedDataByClassTime(arr: SportScheduleInfo[], sort: SortTypes = 'asc') {
+    return arr.sort((item1, item2): any => {
+      if (sort === 'asc') {
+        return item1.classTime - item2.classTime;
+      }
+      return item2.classTime - item1.classTime;
+    });
+  }
+
+  getSportScheduleByName(sportName: string) {
+    this.dataService.getSportScheduleByName(sportName as string).subscribe(
       (res) => {
         this.data = res;
       },
@@ -24,19 +68,7 @@ export class SportsSchedulePageComponent {
     );
   }
 
-  getSportScheduleByName(sportName: string) {
-    this.dataService.getSportScheduleByName(sportName as string).subscribe(
-      (res) => {
-        this.data1 = res;
-      },
-      (err) => {
-        console.error(err, 'Request Error');
-      }
-    );
-  }
-
   ngOnInit(): void {
     this.getSportsList();
-    this.getSportScheduleByName('Boxing');
   }
 }
